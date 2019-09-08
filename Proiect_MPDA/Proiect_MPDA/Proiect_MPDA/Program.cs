e911174d431e1e9aea42959c8ae9d9d09ef03ad8
Mirela Jitianu <mirela.jitianu70@gmail.com>
2019-09-08 21:35:26 +0300

    Add Main

--- Proiect_MPDA/Proiect_MPDA/Proiect_MPDA/Program.cs
+++ Proiect_MPDA/Proiect_MPDA/Proiect_MPDA/Program.cs
@@ -10,6 +10,27 @@ namespace Proiect_MPDA
     {
         static void Main(string[] args)
         {
+            var colas = new Cola(210);
+
+            var lemonLime = new LemonLime(185);
+
+            var rootBeers = new RootBeer(195);
+
+            SodaWater sodaWater = new SodaWater(180);
+            sodaWater.Flavors.Add(colas);
+            sodaWater.Flavors.Add(lemonLime);
+            sodaWater.Flavors.Add(rootBeers);
+
+            sodaWater.DisplayCalories();
+            Console.WriteLine();
+
+
+            // Threshold pentru calorii
+            Drink drinks = new Drink(0);
+            drinks.Attach(new Restaurant("Andrei", 185));
+            drinks.Attach(new Restaurant("Razvan", 180));
+            drinks.Attach(new Restaurant("Marius", 210));
+
         }
     }
 }
