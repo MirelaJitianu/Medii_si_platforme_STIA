3b16f08f346d044bd42cfee86b342e848fecf7d7
Mirela Jitianu <mirela.jitianu70@gmail.com>
2019-09-08 21:40:58 +0300

    add calories per drinks

--- Proiect_MPDA/Proiect_MPDA/Proiect_MPDA/Program.cs
+++ Proiect_MPDA/Proiect_MPDA/Proiect_MPDA/Program.cs
@@ -30,7 +30,33 @@ namespace Proiect_MPDA
             drinks.Attach(new Restaurant("Andrei", 185));
             drinks.Attach(new Restaurant("Razvan", 180));
             drinks.Attach(new Restaurant("Marius", 210));
+            var colas = new Cola(210);
 
+            var lemonLime = new LemonLime(185);
+
+            var rootBeers = new RootBeer(195);
+
+            SodaWater sodaWater = new SodaWater(180);
+            sodaWater.Flavors.Add(colas);
+            sodaWater.Flavors.Add(lemonLime);
+            sodaWater.Flavors.Add(rootBeers);
+
+            sodaWater.DisplayCalories();
+            Console.WriteLine();
+
+
+            // Threshold pentru calorii
+            Drink drinks = new Drink(0);
+            drinks.Attach(new Restaurant("Andrei", 185));
+            drinks.Attach(new Restaurant("Razvan", 180));
+            drinks.Attach(new Restaurant("Marius", 210));
+
+            // Calories for drinks
+            drinks.CaloriesPerDrink = 210; // Cola
+            drinks.CaloriesPerDrink = 185; // LemonLime
+            drinks.CaloriesPerDrink = 195; // RootBeer
+            drinks.CaloriesPerDrink = 180; // SodaWater
+            Console.ReadKey();
         }
     }
 }
